7fd55d14822cfde3f35c40bb574230b141e75b8d
segurakiara791-arte <segurakiara@gmail.com>
2026-05-05 01:13:28 -0500

    reportes

---
+++ ControlEstudiantilSENATI/Form8 Reportes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlEstudiantilSENATI
+{
+    public partial class Form8_Reportes : Form
+    {
+        public Form8_Reportes()
+        {
+            InitializeComponent();
+        }
+    }
+}
