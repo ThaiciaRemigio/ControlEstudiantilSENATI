f04eb1ce47bd9dd45b4d4e02d43e9dd7038276d6
segurakiara791-arte <segurakiara@gmail.com>
2026-05-05 06:21:17 -0500

    Estudiante

---
+++ ControlEstudiantilSENATI/Form9 Configuración.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlEstudiantilSENATI
+{
+    public partial class Form9_Configuración : Form
+    {
+        public Form9_Configuración()
+        {
+            InitializeComponent();
+        }
+    }
+}
