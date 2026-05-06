958fdbcae8e0105b1fee8dd5bb7e45a4bab939f5
segurakiara791-arte <segurakiara@gmail.com>
2026-05-05 20:38:32 -0500

    Actualizado

--- ControlEstudiantilSENATI/Form_PinDeSeguridad.Designer.cs
+++ ControlEstudiantilSENATI/Form_PinDeSeguridad.Designer.cs
@@ -44,8 +44,9 @@
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(2, 1);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(560, 598);
+            this.pictureBox1.Size = new System.Drawing.Size(420, 486);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -53,9 +54,10 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(228, 163);
+            this.pictureBox2.Location = new System.Drawing.Point(188, 141);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(68, 60);
+            this.pictureBox2.Size = new System.Drawing.Size(36, 35);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
@@ -64,29 +66,32 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(139, 239);
+            this.label1.Location = new System.Drawing.Point(98, 188);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(263, 25);
+            this.label1.Size = new System.Drawing.Size(219, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "Ingresar PIN de seguridad";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(108, 276);
+            this.label2.Location = new System.Drawing.Point(81, 224);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(313, 16);
+            this.label2.Size = new System.Drawing.Size(254, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "Por favor, ingresa tu PIN de 4 digitos para continuar.";
             // 
             // textPIN
             // 
-            this.textPIN.Location = new System.Drawing.Point(92, 315);
+            this.textPIN.Location = new System.Drawing.Point(75, 252);
+            this.textPIN.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textPIN.MaxLength = 4;
             this.textPIN.Multiline = true;
             this.textPIN.Name = "textPIN";
             this.textPIN.PasswordChar = '*';
-            this.textPIN.Size = new System.Drawing.Size(346, 43);
+            this.textPIN.Size = new System.Drawing.Size(260, 36);
             this.textPIN.TabIndex = 4;
             this.textPIN.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.textPIN.TextChanged += new System.EventHandler(this.textPIN_TextChanged);
@@ -95,9 +100,10 @@
             // linkOlvidaste
             // 
             this.linkOlvidaste.AutoSize = true;
-            this.linkOlvidaste.Location = new System.Drawing.Point(89, 370);
+            this.linkOlvidaste.Location = new System.Drawing.Point(81, 301);
+            this.linkOlvidaste.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.linkOlvidaste.Name = "linkOlvidaste";
-            this.linkOlvidaste.Size = new System.Drawing.Size(116, 16);
+            this.linkOlvidaste.Size = new System.Drawing.Size(96, 13);
             this.linkOlvidaste.TabIndex = 5;
             this.linkOlvidaste.TabStop = true;
             this.linkOlvidaste.Text = "¿Olvidaste tu PIN?";
@@ -108,9 +114,10 @@
             this.BTNCONTINUAR.BackColor = System.Drawing.Color.DarkBlue;
             this.BTNCONTINUAR.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BTNCONTINUAR.ForeColor = System.Drawing.Color.White;
-            this.BTNCONTINUAR.Location = new System.Drawing.Point(136, 426);
+            this.BTNCONTINUAR.Location = new System.Drawing.Point(102, 333);
+            this.BTNCONTINUAR.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.BTNCONTINUAR.Name = "BTNCONTINUAR";
-            this.BTNCONTINUAR.Size = new System.Drawing.Size(266, 58);
+            this.BTNCONTINUAR.Size = new System.Drawing.Size(200, 47);
             this.BTNCONTINUAR.TabIndex = 7;
             this.BTNCONTINUAR.Text = "Continuar";
             this.BTNCONTINUAR.UseVisualStyleBackColor = false;
@@ -118,9 +125,9 @@
             // 
             // Form_PinDeSeguridad
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(558, 605);
+            this.ClientSize = new System.Drawing.Size(418, 492);
             this.Controls.Add(this.BTNCONTINUAR);
             this.Controls.Add(this.linkOlvidaste);
             this.Controls.Add(this.textPIN);
@@ -128,6 +135,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form_PinDeSeguridad";
             this.Text = "Form_PinDeSeguridad";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
